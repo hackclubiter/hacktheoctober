7d41993b699aa56a16ab1ee8dcf4f5630c3951fe
saswatsamal <samalsaswat0@gmail.com>
2021-09-16 20:51:54 +0530

    [update] : Nav Bar Design

--- js/countdown.js
+++ js/countdown.js
@@ -23,25 +23,3 @@ window.addEventListener("scroll", function () {
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
 
-// hamburger
-
-
-const menuBtn = document.querySelector(".menu-btn");
-const listNav = document.querySelector(".list");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-    if (!menuOpen) {
-        menuBtn.classList.add("open");
-        menuOpen = true;
-        listNav.classList.add("open");
-    }
-    else {
-        closeNav();
-    }
-});
-
-function closeNav() {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-    listNav.classList.remove("open");
-}
--- js/script.js
+++ js/script.js
@@ -118,3 +118,24 @@
   setInterval(draw, 1000 / 30);
   setInterval(update, 1000 / 60);
 }).call(this);
+
+// hamburger
+const menuBtn = document.querySelector(".menu-btn");
+const listNav = document.querySelector(".list");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+        listNav.classList.add("open");
+    }
+    else {
+        closeNav();
+    }
+});
+
+function closeNav() {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    listNav.classList.remove("open");
+}
