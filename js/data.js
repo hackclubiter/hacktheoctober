11036ac8ef20bd78cc2302d8088e93a9c2ab4ecd
saswatsamal <samalsaswat0@gmail.com>
2021-10-01 01:54:43 +0530

    [add] : Project Page Data

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const userData = [
+    {
+        projectName: "coFind",
+        userName: "Saswat Samal",
+        userImage: "https://avatars.githubusercontent.com/u/39031660?v=4",
+        projectRepo:"saswatsamal/cofind",
+    },
+    {
+        projectName: "Chat Application In Java",
+        userName: "Motasim",
+        userImage: "https://avatars.githubusercontent.com/u/44056349?v=4",
+        projectRepo:"motasimmakki/Chat-Application-In-Java",
+    },
+    {
+        projectName: "Get Your PR Accepted",
+        userName: "Samarjeet Banik",
+        userImage: "https://avatars.githubusercontent.com/u/44737783?v=4",
+        projectRepo:"SamarjeetBanik/get-your-PR-accepted",
+    },
+    
+];
---
+++ js/projects.js
@@ -0,0 +1,26 @@
+//User Data Display
+function projectData(user) {
+    return `
+    <div class="project-container">
+    <div class="project-wrapper">
+        <a href="#">
+            <img src="${user.userImage}" alt="${user.userName}" width="150">
+        </a>
+        <div class="project-title">
+        ${user.projectName}
+        </div>
+        <div class="project-place">
+        ${user.userName}
+        </div>
+    </div>
+    <div class="project-icons">
+        <li><a href="https://github.com/${user.projectRepo}" target="blank">Click Here <span class="fas fa-paper-plane"></span></a></li>
+        </li>
+    </div>
+</div>
+  `;
+  }
+  
+  document.getElementById("app").innerHTML = `
+  ${userData.map(projectData).join("")}
+  `;
