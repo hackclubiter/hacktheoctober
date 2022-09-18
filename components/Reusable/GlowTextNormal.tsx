4006c0e647bac9bb6e3a59511453e9a1879eb077
kunal patel <pkunal257@gmail.com>
2022-09-18 16:07:54 +0530

    changes

--- components/Reusable/GlowTextNormal.tsx
+++ components/Reusable/GlowTextNormal.tsx
@@ -7,7 +7,7 @@ type props = {
 
 const GlowTextNormal:React.FC<props> = ({text,size}) => {
   return (
-    <div className={'font-JetBrains relative text-[#E5E1E6] ease-linear duration-150 cursor-pointer selection:bg-[#b4ff39] selection:text-[#170F1E] '+size}>
+    <div className={'font-bold font-JetBrains relative text-[#E5E1E6] ease-linear duration-150 cursor-pointer selection:bg-[#b4ff39] selection:text-[#170F1E] '+size}>
         {text}
         <div className='absolute top-0 blur-[3px] text-netural-500 -z-10'>{text}</div>
     </div>
