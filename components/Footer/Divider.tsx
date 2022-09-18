57d3195bdf3b1ee8065202f38c7c2930ec69230a
kunal patel <pkunal257@gmail.com>
2022-09-18 16:00:39 +0530

    footer initial

---
+++ components/Footer/Divider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Divider = () => {
+  return (
+    <div className="  w-48 h-[0.7px] opacity-50 bg-white"/>
+  )
+}
+
+export default Divider
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,62 @@
+import Link from 'next/link'
 import React from 'react'
+import Divider from './Divider'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex flex-col m-10 md:m-0 md:flex-row  md:justify-center md:items-center">
+   
+
+
+    <div className="flex flex-col space-y-4 mb-10 m-10">
+      <p className="font-JetBrains font-extrabold text-3xl">Hack Club Iter</p>
+      <span className="font-JetBrains">Website</span>
+      <Divider/>
+      <span className="font-JetBrains">Code of Conduct</span>
+      <Divider/>
+      <span className="font-JetBrains">Discord</span>
+      
+    </div>
+
+
+
+    <div className="flex flex-col space-y-4 mb-10 m-10">
+      <p className="font-JetBrains font-extrabold text-3xl">Hack Club Iter</p>
+      <span className="font-JetBrains">Website</span>
+      <Divider/>
+      
+      <span className="font-JetBrains">Code of Conduct</span>
+      <Divider/>
+      <span className="font-JetBrains">Discord</span>
+      
+    </div>
+
+
+    <div className="flex flex-col space-y-4 mb-10 m-10">
+      <p className="font-JetBrains font-extrabold text-3xl">Hack Club Iter</p>
+      <span className="font-JetBrains">Website</span>
+      <Divider/>
+      
+      <span className="font-JetBrains">Code of Conduct</span>
+      <Divider/>
+      
+      <span className="font-JetBrains">Discord</span>
+      
+    </div>
+
+
+    <div className="flex flex-col space-y-4 mb-10 m-10">
+      <p className="font-JetBrains font-extrabold text-3xl">Hack Club Iter</p>
+      <span className="font-JetBrains">Website</span>
+      <Divider/>
+      
+      <span className="font-JetBrains">Code of Conduct</span>
+      <Divider/>
+      
+      <span className="font-JetBrains">Discord</span>
+      
+    </div>
+    </div>
   )
 }
 
