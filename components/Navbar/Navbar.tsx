7e228ca2aca059a00694ea4239ae5bc1d64961df
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-18 15:54:33 +0530

    [added]: glowing text component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import GlowText from '../Reusable/GlowText'
+import GlowTextNormal from '../Reusable/GlowTextNormal'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-20 w-full flex flex-row justify-between items-center px-20 font-JetBrains font-semibold'>
+      <Link href="/">
+        <GlowText
+          text={"HACKTOBERFEST"}
+          size={"text-xl"}
+        />
+        
+      </Link>
+      
+      <div>world</div>
+    </div>
   )
 }
 
