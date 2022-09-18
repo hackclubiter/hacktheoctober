ab7451b218aaac74eeb155e9ee4a747f70044ebe
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-18 18:53:13 +0530

    [fix]: bug fixed

--- components/Reusable/GlowText.tsx
+++ components/Reusable/GlowText.tsx
@@ -9,7 +9,7 @@ const GlowText:React.FC<props> = ({text,size}) => {
   return (
     <div className={'font-JetBrains relative text-[#E5E1E6]/60 hover:text-[#E5E1E6] group ease-linear duration-150 cursor-pointer selection:bg-[#b4ff39] selection:text-[#170F1E] '+size}>
         {text}
-        <div className='absolute top-0 group-hover:blur-[3px] text-netural-500 -z-10'>{text}</div>
+        <div className='absolute top-0 group-hover:blur-[3px] text-netural-500 z-0'>{text}</div>
     </div>
   )
 }
--- components/Reusable/GlowTextNormal.tsx
+++ components/Reusable/GlowTextNormal.tsx
@@ -9,7 +9,7 @@ const GlowTextNormal:React.FC<props> = ({text,size}) => {
   return (
     <div className={'font-bold font-JetBrains relative text-[#E5E1E6] ease-linear duration-150 cursor-pointer selection:bg-[#b4ff39] selection:text-[#170F1E] '+size}>
         {text}
-        <div className='absolute top-0 blur-[3px] text-netural-500 -z-10'>{text}</div>
+        <div className='absolute top-0 blur-[3px] text-netural-500 z-0'>{text}</div>
     </div>
   )
 }
