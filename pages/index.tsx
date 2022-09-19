3bbf9ac672f318746341210f60d2a3087bed6a5d
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-20 01:03:59 +0530

    [added]: Speakers section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Faq from "../components/Faq/Faq";
 import Container from "../components/Layout/Container";
 import HeroSection from "../components/Hero";
 import Prize from "../components/Prize/Prize";
+import Speaker from "../components/Speaker/Speaker";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       {/* Prize */}
       <Prize/>
       {/* Speakers */}
+      <Speaker/>
       {/* Workshops & Events */}
       {/* FAQ */}
       <Faq />
