76c43c8b44da618bdb0b697bc05e7d1e044e24f1
kunal patel <pkunal257@gmail.com>
2022-09-19 10:50:50 +0530

    [modifications]: animation and ui modification

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -17,7 +17,7 @@ const MobileNav:React.FC<props> = ({ menu, setMenu }) => {
         initial={{opacity:0}}
         animate={{opacity:1}}
         transition={{duration:0.6}}
-        className='h-screen w-screen bg-[#170F1E] flex md:hidden flex-col fixed top-0 left-0 z-50'>
+        className='h-screen w-screen  bg-[rgba(0,0,0,0.87)]  backdrop-blur-xl flex md:hidden flex-col fixed top-0 left-0 z-50'>
       <div className='h-32 w-full flex flex-row justify-between items-center px-6 md:px-32 font-JetBrains font-semibold'>
         <Link href='/'>
           <div className='flex flex-row items-center cursor-pointer'>
@@ -36,11 +36,15 @@ const MobileNav:React.FC<props> = ({ menu, setMenu }) => {
       <div className='flex flex-col px-6'>
         {NAV_DATA.map((e, i) => {
           return (
-            <div key={i} className='pb-2 pt-6 border-b border-[#fff]/20'>
+            <motion.div 
+            initial={{translateY:-50,opacity:0}}
+            animate={{translateY:0,opacity:1}}
+            transition={{delay:i/10+0.2, type: "spring", stiffness: 100 }}
+            key={i} className='pb-2 pt-6 border-b border-[#fff]/20'>
               <Link href={e.link}>
                 <GlowText text={e.text} size={"text-xl"} />
               </Link>
-            </div>
+            </motion.div>
           );
         })}
         <div className="absolute bottom-10 uppercase">
