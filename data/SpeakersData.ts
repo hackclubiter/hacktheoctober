0aaea9323abde0db3c63275301577bb31b81efcb
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-20 01:04:36 +0530

    [added]: dummy data for speaker section

---
+++ data/SpeakersData.ts
@@ -0,0 +1,51 @@
+export const SPEAKER_DATA = [
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer',
+        twitter:'https://twitter.com/behara_pavitra',
+        linkedin:'https://www.linkedin.com/in/pavitra-behara/'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+    {
+        name:'Pavitra Behara',
+        image:'https://avatarfiles.alphacoders.com/169/169513.png',
+        description:'Software Developer ',
+        twitter:'#',
+        linkedin:'#'
+    },
+]
