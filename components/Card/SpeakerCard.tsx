894d0f31822ec5f55ef7794fb4dc7e27cf551e01
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-20 01:11:45 +0530

    [fix]: animation in card

--- components/Card/SpeakerCard.tsx
+++ components/Card/SpeakerCard.tsx
@@ -19,9 +19,9 @@ const SpeakerCard: React.FC<props> = ({
   linkedin,
 }) => {
   return (
-    <div className='w-full md:w-72 flex flex-col md:flex-row justify-center items-center hover:scale-[1.05] hover:-translate-y-5 ease-linear duration-150 cursor-pointer'>
+    <div className='w-full md:w-72 flex flex-col md:flex-row justify-center items-center hover:scale-[1.05] hover:-translate-y-2 ease-linear duration-150 cursor-pointer'>
       <div
-        className={`bg-gradient-to-b w-full  flex flex-col items-center m-5 p-2     hover:shadow-xl hover:shadow-cyan-500/50  from-[rgba(124,127,255)] border-4 border-[rgba(64,221,255)] rounded-md to-[rgba(64,221,255)] hover:scale-[1.05] hover:-translate-y-5 ease-linear duration-150 cursor-pointer font-JetBrains relative`}
+        className={`bg-gradient-to-b w-full  flex flex-col items-center m-5 p-2     hover:shadow-xl hover:shadow-cyan-500/50  from-[rgba(124,127,255)] border-4 border-[rgba(64,221,255)] rounded-md to-[rgba(64,221,255)] hover:scale-[1.05] hover:-translate-y-2 ease-linear duration-150 cursor-pointer font-JetBrains relative`}
       >
         <div className=" w-full h-full bg-[url('/bganimated.gif')] mix-blend-color-dodge absolute top-0 bg-cover bg-center"></div>
         <div className='h-44 w-44 rounded-full bg-black overflow-hidden border-2 border-cyan-400 z-10'>
