18122d6448b293977d2ea5e5779979fb5334cbe9
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-20 09:11:40 +0530

    [added]: divider2

--- components/Reusable/Divider.tsx
+++ components/Reusable/Divider.tsx
@@ -8,7 +8,7 @@ const Divider = () => {
       <div className='h-[1px] bg-[#E5E1E6] w-full mt-[1.9rem] ml-3 flex flex-row justify-end relative'>
         <div className='w-32 md:w-52 mx-2 h-1 rounded-sm bg-[#E5E1E6] -mt-4 '></div>
         <div className='w-4 d:w-10 mx-2 h-1 rounded-sm bg-[#E5E1E6] -mt-4 '></div>
-        <div className='w-32 md:w-52 mx-2 h-1 rounded-sm bg-[#E5E1E6] -mt-4 absolute blur-sm right-10'></div>
+        <div className='w-32 md:w-52 mx-2 h-1 rounded-sm bg-[#E5E1E6] -mt-4 absolute blur-sm right-8'></div>
         <div className='w-4 d:w-10 mx-2 h-1 rounded-sm bg-[#E5E1E6] -mt-4 absolute blur-sm '></div>
       </div>
     </div>
